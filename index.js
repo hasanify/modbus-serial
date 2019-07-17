e97ca91ffa628db35f0cf35df8b465623db9b5d1
Hasan Ali <hasanali.abps@gmail.com>
2019-07-17 18:04:14 +0530

    backend for modbus-serial

---
+++ index.js
@@ -0,0 +1,26 @@
+var ModbusRTU = require("modbus-serial");
+var client = new ModbusRTU();
+var app = require('express')();
+var path = require('path');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'hbs');
+
+// open connection to a serial port
+client.connectRTUBuffered("COM9", { baudRate: 9600, parity: 'none' });
+client.setID(1);
+app.get('/read', function(req,res){
+    client.readHoldingRegisters(4, 9, function(err, data) {
+  	res.json(data.data);
+ });
+});
+
+app.get('/', function(req,res){
+	res.render('index', {
+		title: 'Index'
+	})
+});
+
+app.listen(3000, function(){
+  console.log('listening on localhost:3000');
+});
