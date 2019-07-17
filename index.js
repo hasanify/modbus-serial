54fe962c3e4f41bd332e0e76d01ba28d69b51025
Haider Ali <haider.lee23@gmail.com>
2019-07-17 23:12:24 +0530

    Update frontend

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var client = new ModbusRTU();
 var app = require('express')();
 var path = require('path');
 
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // open connection to a serial port
-client.connectRTUBuffered("COM9", { baudRate: 9600, parity: 'none' });
+client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600, parity: 'none' });
 client.setID(1);
-app.get('/read', function(req,res){
+app.get('/read', function(req, res) {
     client.readHoldingRegisters(4, 9, function(err, data) {
-  	res.json(data.data);
- });
+        res.json(data.data);
+    });
 });
 
-app.get('/', function(req,res){
-	res.render('index', {
-		title: 'Index'
-	})
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'E&E Solution'
+    })
 });
 
-app.listen(3000, function(){
-  console.log('listening on localhost:3000');
+app.listen(3000, function() {
+    console.log('listening on localhost:3000');
 });
