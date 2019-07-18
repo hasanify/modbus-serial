d27c8f65acdee8fa053330060102b3561e5cd712
Hasan Ali <hasanali.abps@gmail.com>
2019-07-18 15:40:56 +0530

    update frontend and error handling

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
 var app = require('express')();
 var path = require('path');
+var serialport = require('serialport');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// open connection to a serial port
-client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600, parity: 'none' });
+client.connectRTUBuffered("COM10", { baudRate: 9600, parity: 'none' });
 client.setID(1);
+
+function connect(){
+	client.connectRTUBuffered("COM10", { baudRate: 9600, parity: 'none' });
+	client.setID(1);
+}
+
 app.get('/read', function(req, res) {
     client.readHoldingRegisters(4, 9, function(err, data) {
-        res.json(data.data);
+        if(data == null){connect()}
+       	else{res.json(data.data)}
     });
 });
 
 app.get('/', function(req, res) {
     res.render('index', {
-        title: 'E&E Solution'
+        title: 'E&E Solutions'
     })
 });
 
