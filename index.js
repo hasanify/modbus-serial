57a0dac27a63bf61aaca2b4e53c44c8c3a3e0f2b
Hasan Ali <hasanali.abps@gmail.com>
2019-07-19 16:56:39 +0530

    Update frontend and multiple slaves

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ var app = express();
 var path = require('path');
 var serialport = require('serialport');
 
-client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600, parity: 'none' });
-client.setID(1);
+client.connectRTUBuffered("COM3", { baudRate: 9600, parity: 'none' });
+// client.setID(1);
 
 function connect() {
-    client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600, parity: 'none' });
-    client.setID(1);
+    client.connectRTUBuffered("COM3", { baudRate: 9600, parity: 'none' });
 }
 app.use(express.static('public'));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/read', function(req, res) {
+app.get('/read/:id', function(req, res) {
+	client.setID(parseInt(req.params.id));
     client.readHoldingRegisters(4, 9, function(err, data) {
         if (data == null) { connect() } else { res.json(data.data) }
     });
 });
-
+// process.on('unhandledRejection', up => { throw up })
 app.listen(3000, function() {
     console.log('listening on localhost:3000');
 });
